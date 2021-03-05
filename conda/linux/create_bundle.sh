a2dcb75b89e439506cba24dd241816aced0f635a
lorenz <looooo@users.noreply.github.com>
2021-03-05 15:12:09 +0100

    add some echo commands

--- conda/linux/create_bundle.sh
+++ conda/linux/create_bundle.sh
@@ -1,4 +1,5 @@
 # assume we have a working conda available
+echo "Create the conda environment"
 conda create \
     -p AppDir/usr \
     freecad=0.19 calculix blas=*=openblas gitpython netgen=6.2.1808 \
@@ -12,19 +13,21 @@ conda create \
     -c freecad/label/dev \
     -y
 
+echo "Install the appimage updater addon"
 conda run -p AppDir/usr pip install https://github.com/looooo/freecad.appimage_updater/archive/master.zip
 
-# uninstall some packages not needed
+echo "Uninstall some packages not needed"
 conda uninstall -p AppDir/usr gtk2 gdk-pixbuf llvm-tools \
                               llvmdev clangdev clang clang-tools \
                               clangxx libclang libllvm9 --force -y
 
 version_name=$(conda run -p AppDir/usr python get_freecad_version.py)
 
+echo "Create a list of installed packages"
 conda list -p AppDir/usr > AppDir/packages.txt
 sed -i "1s/.*/\n\nLIST OF PACKAGES:/"  AppDir/packages.txt
 
-# delete unnecessary stuff
+echo "Delete unnecessary stuff"
 rm -rf AppDir/usr/include
 find AppDir/usr -name \*.a -delete
 mv AppDir/usr/bin AppDir/usr/bin_tmp
@@ -40,7 +43,7 @@ sed -i '1s|.*|#!/usr/bin/env python|' AppDir/usr/bin/pip
 rm -rf AppDir/usr/bin_tmp
 #+ deleting some specific libraries not needed. eg.: stdc++
 
-#copy qt.conf
+echo "copy qt.conf"
 cp qt.conf AppDir/usr/bin/
 cp qt.conf AppDir/usr/libexec/
 
@@ -48,7 +51,7 @@ cp qt.conf AppDir/usr/libexec/
 # find . -path "*/__pycache__/*" -delete
 # find . -name "*.pyc" -type f -delete
 
-# reduce size
+echo "reduce size"
 rm -rf AppDir/usr/conda-meta/
 rm -rf AppDir/usr/doc/global/
 rm -rf AppDir/usr/share/gtk-doc/
@@ -57,22 +60,22 @@ rm -rf AppDir/usr/lib/cmake/
 find . -name "*.h" -type f -delete
 find . -name "*.cmake" -type f -delete
 
-# Add libnsl (Fedora 28 and up)
+echo "Add libnsl (Fedora 28 and up)"
 cp ../../libc6/lib/x86_64-linux-gnu/libnsl* AppDir/usr/lib/
 
-# Add documentation
+echo "Add documentation"
 mkdir -p AppDir/usr/share/doc/FreeCAD/
 cp ../../doc/* AppDir/usr/share/doc/FreeCAD/
 
-# mpmath fix:
+echo "Add mpmath fix"
 rm AppDir/usr/lib/python3.8/site-packages/mpmath/ctx_mp_python.py
 cp ../modifications/ctx_mp_python.py AppDir/usr/lib/python3.8/site-packages/mpmath/ctx_mp_python.py
 
-# create the appimage
+echo "Create the appimage"
 chmod a+x ./AppDir/AppRun
 ARCH=x86_64 ../../appimagetool-x86_64.AppImage \
   -u "gh-releases-zsync|FreeCAD|FreeCAD|$DEPLOY_RELEASE|FreeCAD*glibc2.12-x86_64.AppImage.zsync" \
   AppDir  ${version_name}.AppImage
 
-# create hash
+echo "Create hash"
 shasum -a 256 ${version_name}.AppImage > ${version_name}.AppImage-SHA256.txt
